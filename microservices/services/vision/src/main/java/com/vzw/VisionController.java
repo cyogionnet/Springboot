eacae5389cb2bad0238eb9e65c3fa01b6a9f1f4f
Yogendra Chauhan <Yogi@Yogendras-MacBook-Pro.local>
2016-11-11 09:12:00 -0600

    Docker for infra services

--- microservices/services/vision/src/main/java/com/vzw/VisionController.java
+++ microservices/services/vision/src/main/java/com/vzw/VisionController.java
@@ -1,5 +1,7 @@
 package com.vzw;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/vision")
 public class VisionController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(VisionController.class);
+	
 	@Autowired
 	TestService testService;
 	
@@ -19,6 +23,7 @@ public class VisionController {
 	
 	@RequestMapping("/customer/{accountNo}")
 	public String retrieveCustomer(@PathVariable String accountNo) {
+		logger.info("VisionController.retrieveCustomer ------ Getting the Vision response from Vision");
 		String visionResponse = testService.getResponse();
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" + visionResponse);
 		Cache cache = cacheManager.getCache("customerInfo");
@@ -28,6 +33,7 @@ public class VisionController {
 	
 	@RequestMapping("test/{accountNo}")
 	public String viewCustomer(@PathVariable String accountNo){
+		logger.info("VisionController.viewCustomer ------ Getting the Vision response from Cache");
 		Cache cache = cacheManager.getCache("customerInfo");
 		return (String) cache.get(accountNo).get();
 	}
